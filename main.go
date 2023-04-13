3691ee2dc32ef92af0d02fcfbde5dba5aa2a3b1f
behzad <xoltawn@gmail.com>
2023-04-13 17:08:38 +0330

    feat: postgres image changed, connection to db added

--- main.go
+++ main.go
@@ -1,4 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"github.com/xoltawn/simple-file-storage-file-service/domain"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	log.Println(os.Getenv("DB_HOST"))
+	dbPort, err := strconv.Atoi(os.Getenv("DB_PORT"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), dbPort)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = db.AutoMigrate(&domain.File{})
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
