ddc8ac7b2159c2bbc0adc8f3c173d4b199373c20
behzad <xoltawn@gmail.com>
2023-04-14 13:11:33 +0330

    fix: usecase failed test case in TestDownloadFromTextFile grpc handler

--- delivery/grpc/grpc_handler_test.go
+++ delivery/grpc/grpc_handler_test.go
@@ -36,23 +36,23 @@ func TestDownloadFromTextFile(t *testing.T) {
 		assert.Equal(t, sampleErr, err)
 		assert.Equal(t, expRes, res)
 	})
-	// t.Run("if usecase inside grpc handler returns err, return err from handler", func(t *testing.T) {
-	// 	//arrange
-	// 	req := &_filepb.DownloadFromTextFileRequest{}
+	t.Run("if usecase inside grpc handler returns err, return err from handler", func(t *testing.T) {
+		//arrange
+		req := &_filepb.DownloadFromTextFileRequest{}
 
-	// 	bytesToLinksConvertor := _mocks.NewMockBytesToLinksConvertor(ctrl)
-	// 	bytesToLinksConvertor.EXPECT().Convert(gomock.Any()).Return(nil, nil)
-	// 	fileUsecase := _mocks.NewMockFileUsecase(ctrl)
-	// 	fileUsecase.EXPECT().SaveMutltipleFiles(context.TODO(), gomock.Any()).Return(sampleErr)
+		bytesToLinksConvertor := _mocks.NewMockBytesToLinksConvertor(ctrl)
+		bytesToLinksConvertor.EXPECT().Convert(gomock.Any()).Return(nil, nil)
+		fileUsecase := _mocks.NewMockFileUsecase(ctrl)
+		fileUsecase.EXPECT().SaveMutltipleFiles(context.TODO(), gomock.Any()).Return(sampleErr)
 
-	// 	//act
-	// 	sut := _grpc.NewFileGRPCHandler(fileUsecase, bytesToLinksConvertor)
-	// 	_, err := sut.DownloadFromTextFile(context.TODO(), req)
+		//act
+		sut := _grpc.NewFileGRPCHandler(fileUsecase, bytesToLinksConvertor)
+		res, err := sut.DownloadFromTextFile(context.TODO(), req)
 
-	// 	//assert
-	// 	assert.Error(t, err)
-	// 	assert.Equal(t, sampleErr, err)
-	// 	// assert.Equal(t, expRes, res)
-	// })
+		//assert
+		assert.Error(t, err)
+		assert.Equal(t, sampleErr, err)
+		assert.Equal(t, expRes, res)
+	})
 
 }
